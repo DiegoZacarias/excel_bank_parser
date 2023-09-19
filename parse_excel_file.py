f570426ff7797398ebed024ab1f92df063612812
DiegoZacarias <diegozaca97@gmail.com>
2023-09-19 09:39:55 -0400

    feature: parse excel to csv

--- parse_excel_file.py
+++ parse_excel_file.py
@@ -1,14 +1,11 @@
-# import sys
 import pandas as pd
 
-def parse_excel(excel_path):
+def parse_excel(excel_path, target_location):
     
     print(excel_path)
     # Read Excel file
     df = pd.read_excel(excel_path, skiprows=8, usecols='A:F', skipfooter=5)
     print(df)
-    df.to_csv('tests/parsed_file.csv', index=False)
+    df.to_csv(target_location + '/parsed_file.csv', index=False)
     print('The file has been created...')
-
-def test():
-    print('este es un test')
+    print(target_location+'/parsed_file.csv')
---
+++ tests/test_parse_excel.py
@@ -0,0 +1,26 @@
+from parse_excel_file import parse_excel
+import pandas as pd
+import shutil
+import tempfile
+import pytest
+
+@pytest.fixture
+def temp_folder():
+    # Create a temporary folder for testing
+    temp_dir = tempfile.mkdtemp()
+    yield temp_dir  # Provide the temporary folder path to the test
+    # Teardown: Clean up the temporary folder after the test
+    shutil.rmtree(temp_dir)
+
+def test_parse_excel(temp_folder):
+
+    target_location = temp_folder
+    excel_path = 'tests/data/original.xlsx'  
+    parse_excel(excel_path, target_location )
+
+    output_path = target_location+'/parsed_file.csv'
+
+    expected = pd.read_csv('tests/data/expected_output.csv')
+    actual = pd.read_csv(output_path)
+
+    assert actual.equals(expected)
